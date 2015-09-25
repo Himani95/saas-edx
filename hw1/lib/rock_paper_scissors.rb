63aa7c8b50ee0550aad16694d41ef042a01f4515
Nikolaos Kalodimas <nkalodimas@gmail.com>
2015-09-25 20:05:03 +0300

    Added hw1

--- hw1/lib/rock_paper_scissors.rb
+++ hw1/lib/rock_paper_scissors.rb
@@ -2,13 +2,26 @@ class RockPaperScissors
 
   # Exceptions this class can raise:
   class NoSuchStrategyError < StandardError ; end
+	# key wins value
+	@@rules = { 'R' => 'S', 'S' => 'P', 'P' => 'R'}
+	def self.winner(player1, player2)
+		unless @@rules.keys.include?(player1[1]) and @@rules.keys.include?(player2[1])
+			raise	NoSuchStrategyError, "Strategy must be one of R,P,S"
+		end
+		if @@rules[player2[1]] == player1[1]
+			player2
+		else
+			player1
+		end
+	end
 
-  def self.winner(player1, player2)
-    # YOUR CODE HERE
-  end
-
-  def self.tournament_winner(tournament)
-    # YOUR CODE HERE
-  end
+	def self.tournament_winner(tournament)
+		if tournament[0][0].is_a?(String)
+			winner = self.winner(tournament[0], tournament[1])
+		else
+			winner = self.winner(self.tournament_winner(tournament[0]), self.tournament_winner(tournament[1]))
+		end
+		winner
+	end
 
 end
